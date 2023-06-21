4712b6538518304984eff2e1a37f6e41fc77bdc2
Trevor Edwards <trevor.jeffrey.edwards@gmail.com>
2023-06-21 10:27:33 -0400

    Some basic navigation between categories and menus with dummy data

--- meals_app/lib/main.dart
+++ meals_app/lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-
 import 'package:google_fonts/google_fonts.dart';
-
-import 'categories.dart';
+import 'screens/categories_screen.dart';
 
 final theme = ThemeData(
   useMaterial3: true,
@@ -24,7 +22,7 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         theme: theme,
-        home: const CategoriesScreen()
+        home: const CategoriesScreen(),
     );
   }
 }
