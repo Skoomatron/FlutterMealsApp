1ffbd1c85dcc5bbc7adf0ef47903cc4cf44f3591
Trevor Edwards <trevor.jeffrey.edwards@gmail.com>
2023-06-27 10:33:39 -0400

    Added details and ingredient scroll view

--- meals_app/lib/screens/meals_screen.dart
+++ meals_app/lib/screens/meals_screen.dart
@@ -1,42 +1,54 @@
 import 'package:flutter/material.dart';
+import 'package:meals_app/screens/meal_details_screen.dart';
 import 'package:meals_app/widgets/meal_item.dart';
 
 import '../models/meal.dart';
 
 class MealsScreen extends StatelessWidget {
-  const MealsScreen({super.key,required this.title, required this.meals});
+  const MealsScreen({super.key, required this.title, required this.meals});
 
   final String title;
   final List<Meal> meals;
 
+  void selectMeal(BuildContext context, Meal meal) {
+    Navigator.of(context).push(MaterialPageRoute(builder: (ctx) => MealDetailsScreen(meal: meal),),);
+  }
+
   @override
   Widget build(BuildContext context) {
-    Widget content = ListView.builder(itemCount: meals.length, itemBuilder: (ctx, index) => MealItem(meal: meals[index]));
+    Widget content = ListView.builder(
+        itemCount: meals.length,
+        itemBuilder: (ctx, index) => MealItem(
+              meal: meals[index],
+              onSelectMeal: selectMeal,
+            ),
+    );
     if (meals.isEmpty) {
       content = Center(
         child: Column(
           mainAxisSize: MainAxisSize.min,
           children: [
-            Text(
-                'Uh Oh... No Data',
+            Text('Uh Oh... No Data',
                 style: Theme.of(context).textTheme.headlineLarge!.copyWith(
                     color: Theme.of(context).colorScheme.onBackground)),
-            const SizedBox(height: 16,),
+            const SizedBox(
+              height: 16,
+            ),
             Text(
               'Try a different category!',
-              style: Theme.of(context).textTheme.bodyLarge!.copyWith(
-                  color: Theme.of(context).colorScheme.onBackground),
+              style: Theme.of(context)
+                  .textTheme
+                  .bodyLarge!
+                  .copyWith(color: Theme.of(context).colorScheme.onBackground),
             ),
           ],
         ),
       );
     }
     return Scaffold(
-      appBar: AppBar(
-        title: Text(title),
-      ),
-      body: content
-    );
-
+        appBar: AppBar(
+          title: Text(title),
+        ),
+        body: content);
   }
 }
