358055a4f6130efd3890e25e29834052f5656cf7
Trevor Edwards <trevor.jeffrey.edwards@gmail.com>
2023-07-06 12:01:02 -0400

    Added Riverpods for Filters, Favorites, Meals

--- meals_app/lib/screens/meals_screen.dart
+++ meals_app/lib/screens/meals_screen.dart
@@ -5,24 +5,32 @@ import 'package:meals_app/widgets/meal_item.dart';
 import '../models/meal.dart';
 
 class MealsScreen extends StatelessWidget {
-  const MealsScreen({super.key, this.title, required this.meals, required this.onToggleFavorite});
+  const MealsScreen(
+      {super.key,
+      this.title,
+      required this.meals});
 
   final String? title;
   final List<Meal> meals;
-  final void Function(Meal meal) onToggleFavorite;
 
   void selectMeal(BuildContext context, Meal meal) {
-    Navigator.of(context).push(MaterialPageRoute(builder: (ctx) => MealDetailsScreen(meal: meal, onToggleFavorite: onToggleFavorite,),),);
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (ctx) => MealDetailsScreen(
+          meal: meal,
+        ),
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     Widget content = ListView.builder(
-        itemCount: meals.length,
-        itemBuilder: (ctx, index) => MealItem(
-              meal: meals[index],
-              onSelectMeal: selectMeal,
-            ),
+      itemCount: meals.length,
+      itemBuilder: (ctx, index) => MealItem(
+        meal: meals[index],
+        onSelectMeal: selectMeal,
+      ),
     );
     if (meals.isEmpty) {
       content = Center(
@@ -47,7 +55,7 @@ class MealsScreen extends StatelessWidget {
       );
     }
 
-    if (title == null)  {
+    if (title == null) {
       return content;
     }
 
